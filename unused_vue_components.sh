2d832c21a70ab45c2ca1c7203d61cdeb7eab4af4
daniilskr <24217372+daniilskr@users.noreply.github.com>
2023-07-31 15:49:11 +0300

    Update unused_vue_components.sh

--- unused_vue_components.sh
+++ unused_vue_components.sh
@@ -3,12 +3,16 @@
 # Simple and really flawed script to find (potentially) unused vue components
 # Needs some tuning depending on project structure
 
-FILES="resources/js/components/organizms/*"
-for f in $FILES
+for f in $(find "resources/js/components/" -name "*.vue")
 do
     bname=$(basename "$f" ".vue")
+    
+    if [ "$bname" = 'index' ]; then
+        bname=$(dirname "$f" | xargs basename)
+    fi
+
     tname=$(echo "$bname" | sed -r 's/([a-z0-9])([A-Z])/\1-\L\2/g')
-    matches=$(git grep -Pi "(\<$bname|\<$tname)" | grep -v "public/js/app.js:" | grep -v "resources/js/components/organizms/index.js:" | egrep -v "^public/.*")
+    matches=$(git grep -Pi "(\<$bname|\<$tname)" | grep -v "public/js/app.js:" | grep -v "index.js:" | egrep -v "^public/.*")
     cnt=$(echo "$matches" | grep -vc '^$')
     printf "\n\n\n$bname ($cnt)\n"
     echo "$matches"
